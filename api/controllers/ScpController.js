c5bef11d2ec1e4e0f5a7fb2583fb78bba26f1c77
King <kingasawa@gmail.com>
2017-01-12 22:51:45 +0700

    design seller cp template

---
+++ api/controllers/ScpController.js
@@ -0,0 +1,11 @@
+/**
+ * ScpController
+ *
+ * @description :: Server-side logic for managing scps
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
