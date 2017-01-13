51515f2d1c87ff740a0284262729a7cac1290828
Kingasawa <trancatkhanh@gmail.com>
2017-01-12 20:46:57 -0800

    design acp template

--- api/controllers/AcpController.js
+++ api/controllers/AcpController.js
@@ -8,6 +8,34 @@
 module.exports = {
 	index: (req,res) => {
 	  res.view('acp/index')
-  }
+  },
+
+  order: (req,res) => {
+    res.view('acp/order')
+  },
+
+  product: (req,res) => {
+    res.view('acp/product')
+  },
+
+  seller: (req,res) => {
+    res.view('acp/seller')
+  },
+
+  transactions: (req,res) => {
+    res.view('acp/transactions')
+  },
+
+  member: (req,res) => {
+    res.view('acp/member')
+  },
+
+  system: (req,res) => {
+    res.view('acp/system')
+  },
+
+  report: (req,res) => {
+    res.view('acp/report')
+  },
 };
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,14 @@ $(function() {
     $('.menu-arrow.aleft').removeClass('sr-only');
     $('.menu-arrow.aright').addClass('sr-only');
     $('.sidenav').addClass('sidenav-active');
-    $('.head-content').addClass('move-head');
+    $('.head-content .menu-arrow').addClass('move-head');
 
   })
   $('.aleft').click(function(){
     $('.menu-arrow.aright').removeClass('sr-only');
     $('.menu-arrow.aleft').addClass('sr-only');
     $('.sidenav').removeClass('sidenav-active');
-    $('.head-content').removeClass('move-head');
+    $('.head-content .menu-arrow').removeClass('move-head');
 
   })
 });
